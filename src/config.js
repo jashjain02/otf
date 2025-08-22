1318e5ec728a881ed27b8f327c97cb140fa51573
jashjain02 <jashjain28web@gmail.com>
2025-08-23 00:30:43 +0530

    removed the soldout functionality

--- src/config.js
+++ src/config.js
@@ -8,9 +8,6 @@ export const API_ENDPOINTS = {
   EVENT_REGISTRATION: `${API_BASE_URL}/event-registration`,
   REGISTRATIONS: `${API_BASE_URL}/registrations`,
   ORANGETHEORY_REGISTRATION: `${API_BASE_URL}/orangetheory-registration-with-email`,
-  SEND_BULK_EMAILS: `${API_BASE_URL}/send-bulk-emails`,
-  ALL_REGISTRATIONS_SUMMARY: `${API_BASE_URL}/all-registrations-summary`,
-  ALL_REGISTRATIONS: `${API_BASE_URL}/all-registrations`,
 };
 
 export default API_BASE_URL;
